3acefb2af04a4d98a04eefe52d624bc2f08707f6
Jacob Tabor <jtabor214@gmail.com>
2023-11-07 14:17:04 -0500

    Created Employee class and Manager class ; completed #initialize in both

--- employee.rb
+++ employee.rb
@@ -1,5 +1,26 @@
 class Employee
+#super_class
+    attr_reader :name, :title, :salary, :boss
 
-    
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
 
 end
+
+class Manager < Employee
+#sub_class
+    def initialize(name, title, salary, boss)
+        @employees = [] #functionally Employee.boss = manager
+        @employees << Employee if Employee.boss == self.name
+    end
+
+end
+
+employee1 = Employee.new("David", "TA", 10,000, "Darren")
+employee2 = Employee.new("Shawna", "TA", 12,000, "Shawna")
+employee3 = Employee.new("Daren", "TA Manager", 78,000, "Ned")
+employee4 = Employee.new("Ned", "Founder", 1,000,000, nil)
