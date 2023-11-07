be6eb5e88a721eb7b1413d8318349a1815ead495
csmanel <csmanel@gmail.com>
2023-11-07 15:38:47 -0500

    add bonus functionality for employee

--- employee.rb
+++ employee.rb
@@ -7,20 +7,40 @@ class Employee
         @title = title
         @salary = salary
         @boss = boss
+
+        @boss.employees << self if @boss != nil
     end
 
+    def bonus(multiplier)
+        bonus = (self.salary) * multiplier
+        bonus
+    end
 end
 
 class Manager < Employee
 #sub_class
+    attr_reader :employees
+
     def initialize(name, title, salary, boss)
+        super(name, title, salary, boss)
+
         @employees = [] #functionally Employee.boss = manager
-        @employees << Employee if Employee.boss == self.name
     end
 
 end
 
-employee1 = Employee.new("David", "TA", 10,000, "Darren")
-employee2 = Employee.new("Shawna", "TA", 12,000, "Shawna")
-employee3 = Employee.new("Daren", "TA Manager", 78,000, "Ned")
-employee4 = Employee.new("Ned", "Founder", 1,000,000, nil)
+p manager2 = Manager.new("Ned", "Founder", 1000000, nil) #employees
+puts "---"
+p manager1 = Manager.new("Darren", "TA Manager", 78000, manager2)
+puts "---"
+p employee1 = Employee.new("David", "TA", 10000, manager1) 
+puts "---"
+p employee2 = Employee.new("Shawna", "TA", 12000, manager1)
+
+# p manager2.bonus(5) # => 500_000
+# p darren.bonus(4) # => 88_000
+# p david.bonus(3) # => 30_000
+
+
+# if employee we just want their salary
+# if manager we want their salary and those below him 
